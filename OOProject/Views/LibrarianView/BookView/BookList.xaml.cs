f26aa0a6f073a733202a2cd5524773e058a8d1df
Simon Luna <simon.lunapatiarroy@edu.sait.ca>
2024-04-14 11:25:16 -0600

    Attempting to bind the object data across different pages, currently stuck

--- OOProject/Views/LibrarianView/BookView/BookList.xaml.cs
+++ OOProject/Views/LibrarianView/BookView/BookList.xaml.cs
@@ -1,3 +1,4 @@
+using OOProject.Models;
 namespace OOProject.Views.LibrarianView.BookView;
 
 public partial class BookList : ContentPage
@@ -6,4 +7,30 @@ public partial class BookList : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void AddBook_Navigation(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//AddBook");
+    }
+
+    private async void ViewBookDetails_Navigation(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var selectedBook = (Book)button.CommandParameter;
+        await Navigation.PushAsync(new ViewBookDetails(selectedBook));
+        // await Shell.Current.GoToAsync("//ViewBookDetails");
+    }
+
+    private async void EditBookDetails_Navigation(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var selectedBook = (Book)button.CommandParameter;
+        await Navigation.PushAsync(new EditBookDetails(selectedBook));
+        // await Shell.Current.GoToAsync("//EditBookDetails");
+    }
+
+    private void Search_Books(object sender, EventArgs e)
+    {
+
+    }
 }
--- OOProject/Views/LibrarianView/BookView/EditBookDetails.xaml.cs
+++ OOProject/Views/LibrarianView/BookView/EditBookDetails.xaml.cs
@@ -1,9 +1,12 @@
+using OOProject.Models;
+
 namespace OOProject.Views.LibrarianView.BookView;
 
 public partial class EditBookDetails : ContentPage
 {
-	public EditBookDetails()
+	public EditBookDetails(Book book)
 	{
 		InitializeComponent();
+		BindingContext = book;
 	}
 }
