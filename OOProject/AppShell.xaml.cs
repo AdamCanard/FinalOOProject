ea2762fa5d106bd62ba03fab6aa992b9ac8ac702
Simon Luna <simon.lunapatiarroy@edu.sait.ca>
2024-04-13 20:18:24 -0600

    Sorting pathing, moving files into consistent locations

--- OOProject/AppShell.xaml.cs
+++ OOProject/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace OOProject
+﻿using OOProject.Views;
+
+namespace OOProject
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(Login), typeof(Login));
         }
     }
 }
--- OOProject/Views/Login.xaml.cs
+++ OOProject/Views/Login.xaml.cs
@@ -2,8 +2,13 @@ namespace OOProject.Views;
 
 public partial class Login : ContentPage
 {
-	public Login()
+    public Login()
 	{
 		InitializeComponent();
 	}
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        // Navigation using Shell
+        Shell.Current.GoToAsync("//MainPage");
+    }
 }
