157a28b6c027d9008ca16b56be5d924ca44cd7bf
AdamCanard <azcunard@gmail.com>
2024-04-14 16:18:17 -0600

    userlist now displays from database, changed name of Lists.cs added a user manager

---
+++ OOProject/Models/Lists.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOProject;
+
+namespace OOProject.Models
+{
+    // Testing Class used for binding objects and lists
+    public class Lists : BindableObject
+    {
+
+        // This is the only important part of this code
+        public List<Book> BooksList { get; set; } = BookManager.Books;
+        public List<User> UsersList { get; set; } = UserManager.Users;
+        
+    }
+}
