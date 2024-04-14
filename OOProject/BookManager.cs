ddff258cb7bc7c68afeb7946f1a886cee1d9c369
Simon Luna <simon.lunapatiarroy@edu.sait.ca>
2024-04-14 08:59:41 -0600

    Finishing implementing the backend code of the AddBook page

---
+++ OOProject/BookManager.cs
@@ -0,0 +1,23 @@
+﻿using OOProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProject
+{
+    public static class BookManager
+    {
+        public static DatabaseManager Database = new();
+        public static List<Book> Books = new(Database.GetAllBooks());
+
+        public  static void AddNewBook(int isbn, int quant, string title, string author, string cat)
+        {
+            Book newBook = new(isbn, quant, title, author, cat);
+            Database.AddBook(newBook);
+            Books = Database.GetAllBooks();
+            return;
+        }
+    }
+}
--- OOProject/Models/TestingLists.cs
+++ OOProject/Models/TestingLists.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOProject;
 
 namespace OOProject.Models
 {
     // Testing Class used for binding objects and lists
-    class TestingLists : BindableObject
+    public class TestingLists : BindableObject
     {
-        public Book FirstBook { get; set; } = new(1003, 2, "Eragorn", "Who knows", "Fantasy");
+
+        // This is the only important part of this code
+        public List<Book> BooksList { get; set; } = BookManager.Books;
 
         public List<Book> Books { get; set; } =
         [
@@ -33,5 +36,7 @@ namespace OOProject.Models
             new(201, "Simon Luna", "Simon.Luna@sait.ca", "Password", "101 Chapala Grove SE", "Student"),
             new(301, "Ricky Locke", "Ricky.Locke@gmail.com", "Password", "9999 Ave Hell", "Instructor")
         ];
+
+        public Book FirstBook { get; set; } = new(1003, 2, "Eragorn", "Who knows", "Fantasy");
     }
 }
