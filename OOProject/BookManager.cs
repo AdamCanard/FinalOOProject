a8a73c14c97e480205b6d8ab12c21cf7bf7b397b
Tyler Meekel <ty.meekel@gmail.com>
2024-04-17 12:15:33 -0600

    Add remaining methods to BookManager

--- OOProject/BookManager.cs
+++ OOProject/BookManager.cs
@@ -12,12 +12,29 @@ namespace OOProject
         public static DatabaseManager Database = new();
         public static List<Book> Books = new(Database.GetAllBooks());
 
+        public static Book GetBookByISBN(int isbn)
+        {
+            Book retrievedBook = Database.GetBookByISBN(isbn);
+            return retrievedBook;
+        }
+
         public  static void AddNewBook(int isbn, int quant, string title, string author, string cat)
         {
             Book newBook = new(isbn, quant, title, author, cat);
             Database.AddBook(newBook);
             Books = Database.GetAllBooks();
             return;
+        }
+
+        public static void DeleteBook(int isbn)
+        {
+            Database.DeleteBook(isbn);
+        }
+
+        public static void UpdateBook(int isbn, int quant, string title, string author, string cat)
+        {
+            Book updatedBook = new Book(isbn, quant, title, author, cat);
+            Database.UpdateBook(updatedBook);
         }
     }
 }
