384b9241f45fcdefbcecdab2b2629af6cf3f3193
Tyler Meekel <ty.meekel@gmail.com>
2024-04-17 13:00:04 -0600

    add UserManager functionality

--- OOProject/UserManager.cs
+++ OOProject/UserManager.cs
@@ -16,8 +16,38 @@ namespace OOProject
         {
             User newUser = new(library_id, name, email, password, address, account);
             Database.AddUser(newUser);
+            UpdateUserList();
+        }
+
+        public static User GetUserByID(int id)
+        {
+            User retrievedUser = Database.GetUserByID(id);
+            return retrievedUser;
+        }
+
+        public static void DeleteUser(int id)
+        {
+            Database.DeleteUser(id);
+            UpdateUserList();
+        }
+
+        public static void UpdateUser(int userToUpdateID, string? name, string? email, string? password,
+            string? address, string? account)
+        {
+            User userToUpdate = GetUserByID(userToUpdateID);
+            userToUpdate.Name = name ?? userToUpdate.Name;
+            userToUpdate.Email = email ?? userToUpdate.Email;
+            userToUpdate.Password = password ?? userToUpdate.Password;
+            userToUpdate.Address = address ?? userToUpdate.Address;
+            userToUpdate.Account = account ?? userToUpdate.Account;
+            
+            Database.UpdateUser(userToUpdate);
+            UpdateUserList();
+        }
+
+        public static void UpdateUserList()
+        {
             Users = Database.GetAllUser();
-            return;
         }
     }
 }
