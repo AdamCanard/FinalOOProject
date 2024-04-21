08e1aadbc55c6fff81d838f920a289b939d5cba1
Simon Luna <simon.lunapatiarroy@edu.sait.ca>
2024-04-20 19:43:38 -0600

    Adding Exception handling for non numeric ids in login page, also removed confirmation text

--- OOProject/Views/Login.xaml.cs
+++ OOProject/Views/Login.xaml.cs
@@ -15,11 +15,28 @@ public partial class Login : ContentPage
         UserManager.UpdateUserList();
         Password.Text = string.Empty;
         errorMessage.IsVisible = false;
-        confirmationMessage.IsVisible = false;
     }
     private void LoginButton(object sender, EventArgs e)
     {
+        try
+        {
+            int.Parse(ID.Text);
+        }
+        catch (FormatException) 
+        {
+            errorMessage.Text = "ID must be numeric";
+            errorMessage.IsVisible = true;
+            return;
+        }
+        catch (Exception ex) 
+        {
+            errorMessage.Text = ex.Message;
+            errorMessage.IsVisible = true;
+            return;
+        }
+
         DatabaseManager db = new DatabaseManager();
+
         User user = db.GetUserByID(int.Parse(ID.Text));
  
         //User gives correct password for given ID
@@ -46,7 +63,6 @@ public partial class Login : ContentPage
         {
             errorMessage.Text = "Incorrect ID or Password";
             errorMessage.IsVisible = true;
-            confirmationMessage.IsVisible = false;
         }
     }
 }
